1325bed56950c2b5857238e037c0427e508bef75
jeshua hicks <jhicks7884@outlook.com>
2020-06-17 10:34:20 -0400

    fixed user model

--- RateIt/app/models/user.rb
+++ RateIt/app/models/user.rb
@@ -12,14 +12,12 @@ class User < ApplicationRecord
     validates :username, :email, presence: true
     
     def self.from_omniauth(auth)
-        # Creates a new user only if it doesn't exist
         where(email: auth.info.email).first_or_initialize do |user|
-          user.name = auth.info.name
+          #user.username = auth.info.name
           user.email = auth.info.email
+          user.password = SecureRandom.hex
         end
     end
-
-
 end
 
 
