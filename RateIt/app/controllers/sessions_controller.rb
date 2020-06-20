64908d51e01f482d60379067c394b144ca21d3c3
jeshua hicks <jhicks7884@outlook.com>
2020-06-19 21:26:25 -0400

    corrected and following crud and omniauth works

--- RateIt/app/controllers/sessions_controller.rb
+++ RateIt/app/controllers/sessions_controller.rb
@@ -1,33 +1,47 @@
 class SessionsController < ApplicationController
     protect_from_forgery
 
-  def logged_in #Logged user in redirected to user page
-    @user = User.find_by(username: params[:user][:username])
-    if @user.try(:authenticate, params[:user][:password])
-         session[:user_id] = @user.id
-       redirect_to '/users/:id'
+  def new
+  end
 
+  def create #Logged user in redirected to user page
+    if auth_hash = request.env["omniauth.auth"]
+      # They logged into oauth
+      oauth_email = request.env["omniauth.auth"]["info"]["email"]
+     if @user = User.find_by(:email => oauth_email)
+      #raise "existing user".inspect
+      session[:user_id] = @user.id
+      redirect_to new_vehicle_path
+     else
+      
+      user = User.new(:email => oauth_email, :password => SecureRandom.hex, :username => @user.name)
+      if user.save
+       session[:user_id] = @user.id
+       redirect_to vehicle_path
+      else
+        raise user.errors.full_messages.inspect
+      end
+     end
     else
-        redirect_to '/login'
+      #normal login with username and password
+     @user = User.find_by(username: params[:user][:username])
+     if @user.try(:authenticate, params[:user][:password])
+       session[:user_id] = @user.id
+       redirect_to '/users/:id'
+      else
+       render '/users/show'
+      end
     end
   end
 
-  def googleAuth
-    # Get access tokens from the google server
-    access_token = request.env["omniauth.auth"]
-    user = User.from_omniauth(access_token)
-    log_in(user)
-    # Access_token is used to authenticate request made from the rails application to the google server
-    user.google_token = access_token.credentials.token
-    # Refresh_token to request new access_token
-    # Note: Refresh_token is only sent once during the first request
-    refresh_token = access_token.credentials.refresh_token
-    user.google_refresh_token = refresh_token if refresh_token.present?
-    user.save
-    redirect_to root_path
+  def destroy
+    reset_session
+    redirect_to login_path
+  end
+
+  private
+  def auth
+    request.env['omniauth.auth']
   end
 end
-
-
-
 
