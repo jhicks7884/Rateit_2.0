7ec5ab6c271dfca7e7e0c3992bf9db76c2bcb964
jeshua hicks <jhicks7884@outlook.com>
2020-06-19 21:27:22 -0400

    deleted what wasnt needed

--- RateIt/app/models/user.rb
+++ RateIt/app/models/user.rb
@@ -1,24 +1,18 @@
 class User < ApplicationRecord
-  
+
     has_many :vehicles  # those were the vehicles created
-    has_many :ratings   
+    has_many :ratings
    # has_many :vehicles, through: :ratings
 
 
 	has_secure_password
 
     validates :username, :email, uniqueness: true
-    validates(:password, { :length => { :in => 4..8 } })
+    validates(:password, { :length => { :in => 4..166 } })
     validates :username, :email, presence: true
-    
-    def self.from_omniauth(auth)
-        where(email: auth.info.email).first_or_initialize do |user|
-          #user.username = auth.info.name
-          user.email = auth.info.email
-          user.password = SecureRandom.hex
-        end
-    end
+
 end
+
 
 
 
