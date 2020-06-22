485ae3dc45715ef65a4aa46719cfb8982a8a2697
jeshua hicks <jhicks7884@outlook.com>
2020-06-22 17:08:10 -0400

    adding edit and update functions #1

--- RateIt/app/controllers/vehicles_controller.rb
+++ RateIt/app/controllers/vehicles_controller.rb
@@ -1,16 +1,23 @@
 class VehiclesController < ApplicationController
-  
+
    def new
       @vehicle = Vehicle.new
-      
    end
 
-  
+   def edit
+      
+    @vehicle = Vehicle.find_by(:id => [:id])
+   
+   end
+
+   def update
+     raise params.inspect
+   end
+
    def create
       if logged_in?
          #Vehicle.find_by(vehicle_type: params[:vehicle_type])
          @vehicle = Vehicle.create(vehicle_params)
-
          redirect_to '/ratings/new'
       else
          redirect_to '/logout'
@@ -21,16 +28,13 @@ class VehiclesController < ApplicationController
       @vehicles = current_user.vehicles
    end
 
-   def edit
-
-   end
 
    def destroy
 
    end
 
-   
-    private
+
+   private
 
    def vehicle_params
     params.require(:vehicle).permit(:vehicle_type, :style, :model, :make, :year, :user_id)
