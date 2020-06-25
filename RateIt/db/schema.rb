2814041157fe00b25a43570dd0ece342df468f8c
jeshua hicks <jhicks7884@outlook.com>
2020-06-25 13:09:02 -0400

    deleted column

--- RateIt/db/schema.rb
+++ RateIt/db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-  end
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "ratings", force: :cascade do |t|
     t.integer "user_id"
