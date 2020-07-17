7ae50a20cf3816f6a97dc92cab4206e1f44fa099
jeshua hicks <jhicks7884@outlook.com>
2020-07-17 05:25:57 -0400

    fixed redirects

--- RateIt/app/controllers/users_controller.rb
+++ RateIt/app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
       @user = User.new(user_params)
         if @user.save
           session[:user_id] = @user.id
-          redirect_to '/users/:id'
+          redirect_to user_path(@user.id)
         else
           redirect_to '/login'
         end
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
      @user = User.find_by(username: params[:user][:username])
      if @user.try(:authenticate, params[:user][:password])
        session[:user_id] = @user.id
-       redirect_to '/users/:id'
+       redirect_to user_path(@user.id)
       else
        redirect_to '/login'
       end
