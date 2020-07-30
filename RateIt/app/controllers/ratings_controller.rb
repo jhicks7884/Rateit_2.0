0d51387632425f7ec7601519a0f60e7df50af552
jeshua hicks <jhicks7884@outlook.com>
2020-07-30 14:18:17 -0400

    moment vehicle,ratings met

--- RateIt/app/controllers/ratings_controller.rb
+++ RateIt/app/controllers/ratings_controller.rb
@@ -2,7 +2,15 @@ class RatingsController < ApplicationController
     
     def new
       @rating = Rating.new
+      @rating.build_vehicle
+      if params[:vehicle_id] && @vehicle = Vehicle.find_by_id(params[:vehicle_id])
+        @rating = @vehicle.ratings.build
+      else
+        @rating = Rating.new
+        @rating.build_vehicle
+      end
     end
+
     def index
     #  @ = .scoped
     end
@@ -13,17 +21,16 @@ class RatingsController < ApplicationController
 
     def create
       if logged_in?
-       @rating = current_user.ratings.create(rating_params)
-       @rating.save!
-       
-       redirect_to '/ratings/show'
+        @rating = current_user.ratings.create(rating_params)
+        @rating.save
+       redirect_to  '/vehicles'
       else
         redirect_to '/logout'
       end
     end
 
     def update
-    #  @rating = Rating.find(params[:id])
+      @rating = Rating.find(params[:id])
     end
 
     def edit
