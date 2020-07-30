0d73910308bbaae9c3a6ff8e656e0930426c3903
jeshua hicks <jhicks7884@outlook.com>
2020-07-30 14:20:01 -0400

    set so a user cant double rate vehicle

--- RateIt/app/models/rating.rb
+++ RateIt/app/models/rating.rb
@@ -3,4 +3,6 @@ class Rating < ApplicationRecord
   belongs_to :user
   belongs_to :vehicle
 
+  validates :user, uniqueness: { scope: :vehicle, message:"this vehicle has been already been rated"}
+
 end
