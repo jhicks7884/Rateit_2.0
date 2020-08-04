94b62d79f084361a34b497a99772c18de36151d9
jeshua hicks <jhicks7884@outlook.com>
2020-08-04 17:35:21 -0400

    added validations

--- RateIt/app/models/rating.rb
+++ RateIt/app/models/rating.rb
@@ -4,7 +4,8 @@ class Rating < ApplicationRecord
   belongs_to :vehicle
 
   validates :user, uniqueness: { scope: :vehicle, message:"this vehicle has been already been rated"}
-
   
- 
+  validates :vehicle_ratings, presence: true
+
+
 end
