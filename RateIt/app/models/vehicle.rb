fe87d57cdb75476554bdfb11f76d42585f406891
jeshua hicks <jhicks7884@outlook.com>
2020-08-04 17:14:06 -0400

    added validations

--- RateIt/app/models/vehicle.rb
+++ RateIt/app/models/vehicle.rb
@@ -1,4 +1,11 @@
 class Vehicle < ApplicationRecord
+  validates :vehicle_type, presence: true
+  validates :year,  presence: true, length: {minimum: 4}
+  validates :make,  presence: true
+  validates :model,  presence: true
+  validates :style,  presence: true
+  
+
   has_many :ratings
   has_many :users, through: :ratings
   belongs_to :user
